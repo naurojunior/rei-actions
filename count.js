2b95a97109de86e1493ee95529cac7842b191eaf
Nauro <nauro@outlook.com>
2019-08-16 00:20:58 -0300

    Removing dotenv from a file that should not have dotenv

--- count.js
+++ count.js
@@ -1,25 +1,19 @@
-const dotenv = require('dotenv');
 const SlackController = require('./controller/SlackController');
 const EmojiController = require('./controller/EmojiController');
 
-dotenv.config();
-
 async function run(){
-
 	const filename = process.env.FILE_NAME;
 
 	console.log("Requesting...");
 	const newEmojis = await SlackController.fetchEmoji(process.env.SLACK_TOKEN);
-	console.log("Fetched Data");
-
+	
 	console.log("Retrieving from disk...");
 	const oldEmojis = await EmojiController.retrieveFromDisk(filename);
 	const datedEmojis = EmojiController.dateEmojis(newEmojis);
 	
 	console.log("Comparing...");
 	const diffEmoji = EmojiController.getDifference(datedEmojis, oldEmojis);
-	
-	
+		
 	console.log("Saving to disk...");
 	EmojiController.saveToDisk(filename, oldEmojis, diffEmoji);
 	
